6e6d919c9432b15187e193623a3dfa554c2d6afe
DawidWebb <tomasz.matras@developerweb.pl>
2020-07-12 17:08:06 +0200

    add css and fn for show for tools

--- js/showItem.js
+++ js/showItem.js
@@ -7,25 +7,66 @@ class ShowItem extends AddItem {
     this.budget = document.querySelector(".show-data__budget-value");
     this.budgetUp = document.querySelector(".show-data__budget-up");
     this.budgetDown = document.querySelector(".show-data__budget-down");
+    this.budgetListU = document.querySelector(".show-data__list--up");
+    this.budgetListD = document.querySelector(".show-data__list--down");
 
     this.budgetDiv = document.createElement("div");
     this.budget.appendChild(this.budgetDiv);
+
+    // this.edit = document.querySelector(".fa-pencil-alt");
+    // this.del = document.querySelector(".fa-trash-alt");
   }
   showIncomes = () => {
+    const edit = document.createElement("i");
+    edit.className = "fas fa-pencil-alt";
+    const del = document.createElement("i");
+    del.className = "fas fa-trash-alt";
+
     const { name, value, id } = this.plusItem[0];
     const nameItem = document.createElement("li");
     nameItem.className = "item";
     nameItem.style.borderColor = "green";
     nameItem.textContent = `${this.plusItem.length}. ${name}, kwota: ${value} zł`;
-    this.budgetUp.appendChild(nameItem);
+
+    nameItem.appendChild(edit);
+    nameItem.appendChild(del);
+
+    this.budgetListU.appendChild(nameItem);
+
+    // nameItem.addEventListener("click", (nameItem) => this.showTools(nameItem));
   };
   showWidrowth = () => {
+    const edit = document.createElement("i");
+    edit.className = "fas fa-pencil-alt";
+    const del = document.createElement("i");
+    del.className = "fas fa-trash-alt";
+
     const { name, value, id } = this.minusItem[0];
     const nameItem = document.createElement("li");
     nameItem.className = "item";
     nameItem.style.borderColor = "red";
     nameItem.textContent = `${this.minusItem.length}. ${name}, kwota: ${value} zł`;
-    this.budgetDown.appendChild(nameItem);
+
+    nameItem.appendChild(edit);
+    nameItem.appendChild(del);
+
+    this.budgetListD.appendChild(nameItem);
+
+    // nameItem.addEventListener("click", (nameItem) => this.showTools(nameItem));
+  };
+
+  showTools = (item) => {
+    const edit = document.createElement("i");
+    edit.className = "fas fa-pencil-alt";
+    const del = document.createElement("i");
+    del.className = "fas fa-trash-alt";
+    if (item.style.borderColor === "red") {
+      this.budgetListD.appendChild(edit);
+      this.budgetListD.appendChild(del);
+    } else if (item.style.borderColor === "green") {
+      this.budgetListU.appendChild(edit);
+      this.budgetListU.appendChild(del);
+    }
   };
   showBudget = () => {
     let incomes = 0;
